3bb7de84dc5f24f52092ceab9ed9bc7f5cbe4ab6
Arnold DJAPOU <djapoujunior@gmail.com>
2022-03-12 22:47:17 +0100

    Add doctypes and worspcae

---
+++ library_management/library_management/doctype/articl/articl.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Arnold DJAPOU and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Articl', {
+	// refresh: function(frm) {
+
+	// }
+});
