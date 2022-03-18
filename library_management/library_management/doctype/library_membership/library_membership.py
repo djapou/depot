c18c47cb8d99bafbbbaed19b8d010b8b259ebec3
Arnold DJAPOU <djapoujunior@gmail.com>
2022-03-18 17:39:29 +0100

    Add Some controlers to library_management

--- library_management/library_management/doctype/library_membership/library_membership.py
+++ library_management/library_management/doctype/library_membership/library_membership.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2022, Arnold DJAPOU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
 class LibraryMemberShip(Document):
-	pass
+
+	def before_submit(self):
+
+		exists = frappe.db.exists(
+			"Library MemberShip",
+			{
+				"library_member": self.library_member,
+				"docstatus": DocStatus.submitted(),
+				"to_today": (">", self.from_date),
+			},
+		)
+		if exists:
+			frappe.throw("Un abonnement actif existe deja pour ce membre")
+
+		period_pret = frappe.db.get_single_value("Library Settings", "period_pret")
+		self.to_today = frappe.utils.add_days(self.from_date, period_pret or 30)
+
--- library_management/library_management/doctype/library_transaction/library_transaction.py
+++ library_management/library_management/doctype/library_transaction/library_transaction.py
@@ -1,8 +1,62 @@
 # Copyright (c) 2022, Arnold DJAPOU and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.model.docstatus import DocStatus
 
 class LibraryTransaction(Document):
-	pass
+
+	def before_submit(self):
+		if self.type == "Emission":
+			self.emission_article()
+			self.validation_max_limit()
+			article = frappe.get_doc("Articl", self.article)
+			article.statut = "Emis"
+			article.save()
+
+		elif self.type == "Retour":
+			self.retour_article()
+			article = frappe.get_doc("Articl", self.article)
+			article.statut = "Disponible"
+			article.save()
+
+	def emission_article(self):
+		self.member_exists()
+		article = frappe.get_doc("Articl", self.article)
+
+		if article.statut == "Emis":
+			frappe.throw("L\'article a deja ete emis par un autre membre")
+
+	def retour_article(self):
+		article = frappe.get_doc("Articl", self.article)
+
+		if article.statut == "Disponible":
+			frappe.throw("L\'article ne peut pas etre retourne sans au prealable etre emis")
+
+	def validation_max_limit(self):
+		max_number = frappe.db.get_single_value("Library Settings", "max_number")
+		count = frappe.db.count(
+			"Library Transaction",
+			{
+				"library_member": self.library_member,
+				"type": "Emission",
+				"docstatus": DocStatus.submitted(),
+			}
+		)
+		if count >= max_number:
+			frappe.throw("La limite maximum d\'emission des articles a ete atteint")
+
+
+	def member_exists(self):
+		exists = frappe.db.exists(
+			"Library MemberShip",
+			{
+				"library member": self.library_member,
+				"docstatus": DocStatus.submitted(),
+				"from_date": ("&lt;", self.date),
+				"to_today": (">", self.date),
+			},
+		)
+		if not exists:
+			frappe.throw("Ce membre n\'a pas d\'abonnement actif")
